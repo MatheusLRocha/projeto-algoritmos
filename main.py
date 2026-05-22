c3b1855a7e96edbaa7dd14a55df8333fa9130fa7
MatheusLRocha <matheuslrocha10@gmail.com>
2026-05-22 14:30:58 -0300

    Arquivo principal para inicializar o projeto criado
    Arquivo main foi criado para gerenciar a execução do projeto todo

--- main.py
+++ main.py
@@ -2,5 +2,12 @@
 
 # Importa uma única vez todas as bibliotecas e módulos que vão ser usados no projeto
 
-# from PasswordManager import PasswordService
-# from PasswordBank import PasswordRepository
+from PasswordManager import PasswordService
+from PasswordBank import PasswordRepository
+from TelaBloqueio import TelaLogin
+
+banco = PasswordRepository()
+pm = PasswordService(banco)
+
+if __name__ == '__main__':
+    TelaLogin(pm)
