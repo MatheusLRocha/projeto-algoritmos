4a900901cb4f7650d4935092448cb44fef79bb2f
MatheusLRocha <matheuslrocha10@gmail.com>
2026-05-23 18:38:14 -0300

    Correções no banco
    Correções de nomes e lógica feitas no banco de dados, agora salt e token de segurança estão mais seguros

--- PasswordBank.py
+++ PasswordBank.py
@@ -1,9 +1,9 @@
-from database import Password, Authenticantion, db # Importa a classe Password e o banco de dados do arquivo database.py
+from database import Password, Authentication, db # Importa a classe Password e o banco de dados do arquivo database.py
 
 class PasswordRepository:
     def __init__(self):
         db.connect() # Conecta ao banco de dados
-        db.create_tables([Password, Authenticantion]) # Cria a tabela de senhas no banco de dados, caso ela ainda não exista
+        db.create_tables([Password, Authentication]) # Cria as tabelas no banco de dados, caso ela ainda não exista
 
     # CRUD(Create, Read, Update, Delete) para manipular os dados do banco de dados utilizando a classe Password
 
@@ -31,3 +31,11 @@ class PasswordRepository:
     # Deleta completamente todos os dados da tabela para criar uma nova de acordo com o cadastro
     def new_register(self):
         Password.delete().execute()
+
+    # Deleta completamente a autenticação e o salt para criar uma nova de acordo com o cadastro
+    def new_auth(self):
+        Authentication.delete().execute()
+
+    # Adiciona o salt e o token de segurança para entrar na conta
+    def add_auth(self, salt, safe_token):
+        Authentication.create(salt=salt, auth=safe_token)
--- database.py
+++ database.py
@@ -5,16 +5,16 @@ from peewee import *
 db = SqliteDatabase('passwords.db') # Gera um arquivo para armazenar o banco de dados
 
 #Modelo base para todas as tabelar herdarem a mesma configuração de banco
-class BaseModel(Model):
+class BaseModel(Model): # Model é a classe base do peewee para criar tabelas
     # Classe Meta para configurar a tabela
     class Meta:
         database = db # Define o banco de dados a ser utilizado
 
 # Cria a tabela que irá armazenar as senhas
-class Password(BaseModel): # Model é a classe base do peewee para criar tabelas
+class Password(BaseModel): 
     title = CharField()
     password = TextField()
 
-class Authenticantion(BaseModel):
+class Authentication(BaseModel):
     salt = BlobField()
     auth = CharField(max_length=255)
